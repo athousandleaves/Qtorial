83202f102762546fcb32ed073d7f60176082449d
athousandleaves <superamare@gmail.com>
2017-09-20 13:51:32 -0500

    register local variables for currentUser, error and success messages

--- app.js
+++ app.js
@@ -110,3 +110,10 @@ app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
+
+app.use(function (req, res, next){
+    res.locals.currentUser = req.user;
+    res.locals.error = req.flash('error');
+    res.locals.success = req.flash('success');
+    next();
+});
