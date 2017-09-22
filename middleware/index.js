923e3575bc78556e69e7d9ace3261609c0032c44
athousandleaves <superamare@gmail.com>
2017-09-22 14:20:21 -0500

    set up tutorial owner auth function

--- middleware/index.js
+++ middleware/index.js
@@ -2,3 +2,23 @@ var Tutorial = require("../models/tutorial");
 var Comment = require("../models/comment");
 
 var middlewareObj = {};
+
+middlewareObj.isTutorialOwner = function(req, res, next) {
+    if (req.isAuthenticated()) {
+      Tutorial.findById(req.params.id, function(err, foundTutorial) {
+        if (err) {
+          res.redirect("back");
+        } else {
+          //does user own tutorial?
+          if (foundTutorial.author.id.equals(req.user._id)) {
+            next();
+          } else {
+            req.flash("error", "You don't have permission to do that.");
+            res.redirect("back");
+          }
+        }
+      });
+    } else {
+      res.redirect("back");
+    }
+  };
