b9942098dea0758ff79780f7fddfed4c1e6bcf1d
athousandleaves <superamare@gmail.com>
2017-09-22 08:16:46 -0500

    set up register route

--- routes/index.js
+++ routes/index.js
@@ -9,8 +9,21 @@ var hbs = require("express-handlebars");
 // AUTH ROUTES
 //================
 
-//show register form
+  //show register form
 router.get("/register", function(req, res) {
-    res.render("register");
+  res.render("register");
+});
+
+  //handle sign up logic
+router.post("/register", function(req, res) {
+  var newUser = new User({ username: req.body.username });
+  User.register(newUser, req.body.password, function(err, user) {
+    if (err) {
+      console.log(err);
+      return res.render("register");
+    }
+    passport.authenticate("local")(req, res, function() {
+      res.redirect("/");
+    });
   });
-  
+});
