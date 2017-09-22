5a6c66891a4b377bd1d19e2ed84de4fbce2a8c35
athousandleaves <superamare@gmail.com>
2017-09-22 08:46:37 -0500

    set up show page route

--- routes/tutorials.js
+++ routes/tutorials.js
@@ -5,3 +5,20 @@ var Comment = require("../models/comment");
 var topic = require("../models/topic");
 var middleware = require("../middleware");
 var request = require("request");
+
+// show page
+router.get("/", function(req, res) {
+    topic.find({}, function(err, alltopics) {
+      if (err) {
+        console.log(err);
+      } else {
+        tutorial.find({}, function(err, alltutorials) {
+          if (err) {
+            console.log(err);
+          } else {
+            res.render("home", { topics: alltopics, tutorials: alltutorials });
+          }
+        });
+      }
+    });
+  });
