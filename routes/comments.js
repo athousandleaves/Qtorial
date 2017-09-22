8255e931a113567f9f0fb7278fd52f1bc36368c6
athousandleaves <superamare@gmail.com>
2017-09-22 08:32:54 -0500

    add new comments route

--- routes/comments.js
+++ routes/comments.js
@@ -3,3 +3,15 @@ var router = express.Router({ mergeParams: true });
 var tutorial = require("../models/tutorial");
 var Comment = require("../models/comment");
 var middleware = require("../middleware");
+
+// Comments new
+router.get("/tutorials/:id/comments/new", middleware.isLoggedIn, function(req, res) {
+    //find tutorial by ID
+    tutorial.findById(req.params.id, function(err, tutorial) {
+      if (err) {
+        console.log(err);
+      } else {
+        res.render("comments/new", { tutorial: tutorial });
+      }
+    });
+  });
