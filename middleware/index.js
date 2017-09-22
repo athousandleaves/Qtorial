4b272d239a3e10e15debd869e297919780dca811
athousandleaves <superamare@gmail.com>
2017-09-22 14:21:04 -0500

    set up comment owner auth function

--- middleware/index.js
+++ middleware/index.js
@@ -9,11 +9,31 @@ middlewareObj.isTutorialOwner = function(req, res, next) {
         if (err) {
           res.redirect("back");
         } else {
-          //does user own tutorial?
+          // does user own tutorial?
           if (foundTutorial.author.id.equals(req.user._id)) {
             next();
           } else {
             req.flash("error", "You don't have permission to do that.");
+            res.redirect("back");
+          }
+        }
+      });
+    } else {
+      res.redirect("back");
+    }
+  };
+
+  middlewareObj.isCommentOwner = function(req, res, next) {
+    if (req.isAuthenticated()) {
+      Comment.findById(req.params.comment_id, function(err, foundComment) {
+        if (err) {
+          res.redirect("back");
+        } else {
+          // does user own comment?
+          if (foundComment.author.id.equals(req.user._id)) {
+            next();
+          } else {
+            req.flash("error", "You need to be logged in to do that!");
             res.redirect("back");
           }
         }
