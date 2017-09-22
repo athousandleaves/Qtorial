58ac9feca72fb530239600c9204ac16d41d95838
athousandleaves <superamare@gmail.com>
2017-09-22 08:19:20 -0500

    set up logout route

--- routes/index.js
+++ routes/index.js
@@ -9,12 +9,12 @@ var hbs = require("express-handlebars");
 // AUTH ROUTES
 //================
 
-  //show register form
+  // show register form
 router.get("/register", function(req, res) {
   res.render("register");
 });
 
-  //handle sign up logic
+  // handle sign up logic
 router.post("/register", function(req, res) {
   var newUser = new User({ username: req.body.username });
   User.register(newUser, req.body.password, function(err, user) {
@@ -28,12 +28,12 @@ router.post("/register", function(req, res) {
   });
 });
 
-  //show login form
+  // show login form
 router.get("/login", function(req, res) {
     res.render("login");
   });
   
-  //handle login logic
+  // handle login logic
 router.post(
   "/login",
   passport.authenticate("local", {
@@ -42,3 +42,10 @@ router.post(
   }),
   function(req, res) {}
 );
+
+  // logout route
+router.get("/logout", function(req, res) {
+  req.logout();
+  req.flash("success", "Logged out successfully!");
+  res.redirect("/");
+});
