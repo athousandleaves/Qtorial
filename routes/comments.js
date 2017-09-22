573cb3cdf7ffc88568baf50c64bea38755ea49f5
athousandleaves <superamare@gmail.com>
2017-09-22 08:34:56 -0500

    add post comment route

--- routes/comments.js
+++ routes/comments.js
@@ -15,3 +15,32 @@ router.get("/tutorials/:id/comments/new", middleware.isLoggedIn, function(req, r
       }
     });
   });
+
+// Comments create
+router.post("/tutorials/:id/comments/", middleware.isLoggedIn, function(req, res) {
+    // look up tutorial by ID
+    tutorial.findById(req.params.id, function(err, tutorial) {
+      if (err) {
+        console.log(err);
+        res.redirect("home");
+      } else {
+        Comment.create(req.body.comment, function(err, comment) {
+          if (err) {
+            console.log(err);
+          } else {
+            //add username and id to comment
+            comment.author.id = req.user._id;
+            comment.author.username = req.user.username;
+            //save comment
+            comment.save();
+            tutorial.comments.push(comment);
+            tutorial.save();
+            res.redirect("/tutorials/" + tutorial._id);
+          }
+        });
+      }
+    });
+    // create comment
+    // connect new comment to tutorial
+    // redirect to tutorial show page
+  });
